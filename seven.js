1af83906671831e274572492c373e32865b9eb25
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-07-27 22:33:33 +0600

    q-6(task 7) done

---
+++ seven.js
@@ -0,0 +1,57 @@
+const readline = require('readline');
+
+const readData = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readData.question('Enter a Roman numeral: ', (romanNumeral) => {
+
+    const romanToInt = romanValue => {
+        romanValue = romanValue.toUpperCase();
+
+        const romanNumerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        };
+
+        if (!romanValue || romanValue.trim() === '') {
+            throw new Error('Invalid input: Empty Roman numeral.');
+        }
+
+        let result = 0;
+
+        for (let i = 0; i < romanValue.length; i++) {
+            const currentNumeral = romanValue[i];
+            const currentValue = romanNumerals[currentNumeral];
+            const nextNumeral = romanValue[i + 1];
+            const nextValue = romanNumerals[nextNumeral];
+
+            if (!currentValue) {
+                throw new Error(`Invalid input: Character "${currentNumeral}" is not a valid Roman numeral.`);
+            }
+
+            if (nextValue > currentValue) {
+                if (![1, 10, 100].includes(currentValue)) {
+                    throw new Error(`Invalid input: Invalid sequence "${currentNumeral}${nextNumeral}".`);
+                }
+                result += (nextValue - currentValue);
+                i++; // Skip the next numeral since it's already accounted for
+            } else {
+                result += currentValue;
+            }
+        }
+
+        return result;
+    };
+
+    const result = romanToInt(romanNumeral);
+
+    console.log(`The corresponding integer value is: ${result}`);
+    readData.close();
+});
